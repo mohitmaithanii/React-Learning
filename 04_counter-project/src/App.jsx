04696492d4fc4c7aeeb724a5d002d5d06c1f504f
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-02-29 13:59:42 +0530

    counter project in react

---
+++ 04_counter-project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [counter, setCounter] = useState(0);
+
+	const addValue = () => {
+		setCounter(counter + 1);
+	};
+
+	const removeValue = () => {
+		// Check if count is greater than 0 before decrementing
+		if (counter > 0) {
+			setCounter(counter - 1);
+		}
+	};
+
+	return (
+		<>
+			<h1>Chai aur React - Project</h1>
+			<h2>Counter value : {counter}</h2>
+
+			<button onClick={addValue}>Add value</button>
+			<br />
+			<br />
+			<button onClick={removeValue}>remove value</button>
+		</>
+	);
+}
+
+export default App;
