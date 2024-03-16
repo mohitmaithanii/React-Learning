354cc6373643c187356d2f99423a97563878eb02
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-03-16 22:32:31 +0530

    redux toolkit

--- 13_todo-reduxToolkit/src/App.jsx
+++ 13_todo-reduxToolkit/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
 	return (
 		<>
 			<h1 className="text-3xl"> Learn Redux Toolkit</h1>
+			<AddTodo />
+			<Todos />
 		</>
 	);
 }
--- 13_todo-reduxToolkit/src/features/todo/todoSlice.js
+++ 13_todo-reduxToolkit/src/features/todo/todoSlice.js
@@ -1,12 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-	todos: [
-		{
-			id: 1,
-			text: "Hello world",
-		},
-	],
+	todos: [{ id: 1, text: "Hello world" }],
 };
 
 export const todoSlice = createSlice({
