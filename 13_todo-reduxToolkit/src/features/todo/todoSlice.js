1a756cf30f7ca8f568aa6dc25c3b35c19271ae7b
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-03-17 23:32:13 +0530

    add some notes in todoSlice.js

--- 13_todo-reduxToolkit/src/features/todo/todoSlice.js
+++ 13_todo-reduxToolkit/src/features/todo/todoSlice.js
@@ -1,26 +1,32 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+// Initial state of the todos array
 const initialState = {
 	todos: [{ id: 1, text: "Hello world" }],
 };
 
+// Create a new slice of the Redux store called "todo"
 export const todoSlice = createSlice({
 	name: "todo",
 	initialState,
 	reducers: {
+		// Function to add a new todo to the todos array
 		addTodo: (state, action) => {
 			const todo = {
-				id: nanoid(),
-				text: action.payload,
+				id: nanoid(), // Function to add a new todo to the todos array
+				text: action.payload, // Get the text of the new todo from the action payload
 			};
-			state.todos.push(todo);
+			state.todos.push(todo); // Add the new todo to the todos array
 		},
+		// Function to remove a todo from the todos array
 		removeTodo: (state, action) => {
 			state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+			// Filter out the todo with the matching ID from the todos array
 		},
 	},
 });
 
+// Export the addTodo and removeTodo action creators
 export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
